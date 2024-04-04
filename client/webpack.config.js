106de8bf945a84960e1af7c4fb3872171cc0cb1b
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-04 15:30:10 -0400

    Adding a babel loader in order for users to access our web even on older devices

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,7 +26,22 @@ module.exports = () => {
     ],
 
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "defaults" }]],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
     },
   };
 };
