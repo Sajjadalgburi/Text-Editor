c7769ce4d34464dd59802ca0dd0e28a3e9c78104
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-06 18:47:21 -0400

    created function to get all the notes form the database and serve them onto user

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,6 +12,7 @@ const initdb = async () =>
     },
   });
 
+// function to update the text editor and store the val in dbs when user clicks out of it
 export const putDb = async (content) => {
   try {
     // Open the database
@@ -37,7 +38,30 @@ export const putDb = async (content) => {
   }
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+// function to get all the notes form the database and serve them onto user
+export const getDb = async () => {
+  try {
+    // Open the database
+    const database = await openDB("jate", 1);
+
+    // Start a transaction
+    const tx = database.transaction("JATE-vault", "readonly");
+
+    // Access the object store
+    const store = tx.objectStore("JATE-vault");
+
+    // Add content to the object store
+    const request = store.getAll();
+
+    // Wait for the operation to complete
+    const results = await request;
+
+    return results;
+  } catch (error) {
+    // Handle any errors that occur during the database operation
+    console.error("Error adding content to database:", error);
+    throw error;
+  }
+};
 
 initdb();
