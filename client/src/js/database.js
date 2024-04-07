c17ea5a333be3096a6797051e5eafb86017e75a9
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-06 20:28:08 -0400

    trying to debug by removing unneeded things

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,65 +1,42 @@
 import { openDB } from "idb";
 
-const initdb = async () =>
-  openDB("jate", 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
-        return;
-      }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
-    },
-  });
+const initdb = async () => {
+  try {
+    const database = await openDB("jate", 1, {
+      upgrade(db) {
+        if (db.objectStoreNames.contains("jate")) {
+          console.log("jate database already exists");
+          return;
+        }
+        db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+        console.log("jate database created");
+      },
+    });
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    throw error;
+  }
+};
 
-// function to update the text editor and store the val in dbs when user clicks out of it
 export const putDb = async (content) => {
   try {
-    // Open the database
     const database = await openDB("jate", 1);
-
-    // Start a transaction
-    const tx = database.transaction("jate", "readwrite");
-
-    // Access the object store
-    const store = tx.objectStore("jate");
-
-    // Add content to the object store
-    const request = store.put({ content: content });
-
-    // Wait for the operation to complete
-    const results = await request;
-
-    return results;
+    const store = database.transaction("jate", "readwrite").objectStore("jate");
+    await store.put({ content: content });
   } catch (error) {
-    // Handle any errors that occur during the database operation
     console.error("Error adding content to database:", error);
     throw error;
   }
 };
 
-// function to get all the notes form the database and serve them onto user
 export const getDb = async () => {
   try {
-    // Open the database
     const database = await openDB("jate", 1);
-
-    // Start a transaction
-    const tx = database.transaction("jate", "readonly");
-
-    // Access the object store
-    const store = tx.objectStore("jate");
-
-    // Add content to the object store
-    const request = store.getAll();
-
-    // Wait for the operation to complete
-    const results = await request;
-
+    const store = database.transaction("jate", "readonly").objectStore("jate");
+    const results = await store.getAll();
     return results;
   } catch (error) {
-    // Handle any errors that occur during the database operation
-    console.error("Error adding content to database:", error);
+    console.error("Error retrieving content from database:", error);
     throw error;
   }
 };
